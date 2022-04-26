5ea0fd4efed6ad158495394dec5f5db682968422
David Andersson <lejonmanen@gmail.com>
2022-04-26 10:43:55 +0200

    Toggle dark mode

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
+
 // En komponent som kan ändra tema mellan ljust och mörkt.
-
 const Theme = () => {
+	// i Vue hade vi använt en data property
+	// i React använder vi state variabler
+	const [theme, setTheme] = useState<string>('light')
 
+	const clickDark = () => setTheme('dark')
+	const clickLight = () => setTheme('light')
 
 	return (
-		<div className="theme light">
+		<div className={"theme " + theme}>
 			Välj tema: 
-			<button> ljust tema </button>
-			<button> mörkt tema </button>
+			<button onClick={clickLight}> ljust tema </button>
+			<button onClick={clickDark}> mörkt tema </button>
 		</div>
 	)
 }
