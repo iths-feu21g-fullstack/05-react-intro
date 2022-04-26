ad0fb6f095d5a356a3fd6f68c546e737f99eb5ab
David Andersson <lejonmanen@gmail.com>
2022-04-26 10:05:40 +0200

    Started on Theme component

---
+++ src/components/Theme.tsx
@@ -0,0 +1,15 @@
+
+
+const Theme = () => {
+
+
+	return (
+		<div className="theme light">
+			Välj tema: 
+			<button> ljust tema </button>
+			<button> mörkt tema </button>
+		</div>
+	)
+}
+
+export default Theme
